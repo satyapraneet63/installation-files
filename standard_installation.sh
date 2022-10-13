250a5349799b7ac1e4107dbf21d29f01134ab53e
satyapraneet63 <114915246+satyapraneet63@users.noreply.github.com>
2022-10-13 16:45:32 +0530

    Update standard_installation.sh

--- standard_installation.sh
+++ standard_installation.sh
@@ -6,21 +6,21 @@ sudo apt-get update
 
 #Zoom installing
 wget https://zoom.us/client/5.12.2.4816/zoom_amd64.deb
-sudo dpkg -i ~/Downloads/zoom_amd64.deb
+sudo dpkg -i ~/zoom_amd64.deb
 
 #slack installing
 wget https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
-sudo dpkg -i ~/Downloads/slack-desktop-4.28.184-amd64.deb
+sudo dpkg -i ~/slack-desktop-4.28.184-amd64.deb
 
 #sudo apt install git git-lfs -y
 
 #MySQL Workbench
 wget https://github.com/satyapraneet63/installation-files/raw/main/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
-sudo dpkg -i ~/Downloads/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
+sudo dpkg -i ~/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
 
 #Any_Desk Installing
 wget https://download.anydesk.com/linux/anydesk_6.2.0-1_amd64.deb
-sudo dpkg -i ~/Downloads/anydesk_6.2.0-1_amd64.deb
+sudo dpkg -i ~/anydesk_6.2.0-1_amd64.deb
 
 #intellij IDE
 sudo snap install intellij-idea-community --classic
