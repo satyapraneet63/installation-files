dce2d36b7a7259ddd05e647811b599798c84402b
satyapraneet63 <114915246+satyapraneet63@users.noreply.github.com>
2022-10-14 12:17:14 +0530

    Update standard_installation.sh

--- standard_installation.sh
+++ standard_installation.sh
@@ -11,22 +11,22 @@ echo deb http://archive.ubuntu.com/ubuntu/ bionic universe | sudo tee /etc/apt/s
 sudo apt-get update
 
 #Zoom installing
-wget https://zoom.us/client/5.12.2.4816/zoom_amd64.deb
-sudo dpkg -i ~/zoom_amd64.deb
+wget https://zoom.us/client/5.12.2.4816/zoom_amd64.deb -P /tmp
+sudo dpkg -i /tmp/zoom_amd64.deb
 
 #slack installing
-wget https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
-sudo dpkg -i ~/slack-desktop-4.28.184-amd64.deb
+wget https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb -P /tmp
+sudo dpkg -i /tmp/slack-desktop-4.28.184-amd64.deb
 
 #sudo apt install git git-lfs -y
 
 #MySQL Workbench
-wget https://github.com/satyapraneet63/installation-files/raw/main/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
-sudo dpkg -i ~/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
+wget https://github.com/satyapraneet63/installation-files/raw/main/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb -P /tmp
+sudo dpkg -i /tmp/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
 
 #Any_Desk Installing
-wget https://download.anydesk.com/linux/anydesk_6.2.0-1_amd64.deb
-sudo dpkg -i ~/anydesk_6.2.0-1_amd64.deb
+wget https://download.anydesk.com/linux/anydesk_6.2.0-1_amd64.deb -P /tmp
+sudo dpkg -i /tmp/anydesk_6.2.0-1_amd64.deb
 
 #intellij IDE
 sudo snap install intellij-idea-community --classic
