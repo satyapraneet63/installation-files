88977f220947cb78cc522d4b9aaf12fdd91eebbc
satyapraneet63 <114915246+satyapraneet63@users.noreply.github.com>
2022-10-14 12:56:39 +0530

    Update standard_installation.sh

--- standard_installation.sh
+++ standard_installation.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -eu -o pipefail # fail on error and report it, debug all lines
+#set -eu -o pipefail # fail on error and report it, debug all lines
 
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
 sudo apt-get update
-sudo apt-get upgrade -y
-echo deb http://archive.ubuntu.com/ubuntu/ bionic universe | sudo tee /etc/apt/sources.list.d/bionic.list
-sudo apt-get update
+sudo apt-get upgrade
+sudo apt install libpangox-1.0-0 libpython2.7-minimal libpython2.7-stdlib libzip4 libtinfo5 libpython2.7 libgtkglext1
+#echo deb http://archive.ubuntu.com/ubuntu/ bionic universe | sudo tee /etc/apt/sources.list.d/bionic.list
+#sudo apt-get update
 
 #Zoom installing
 wget https://zoom.us/client/5.12.2.4816/zoom_amd64.deb -P /tmp
@@ -19,11 +20,10 @@ wget https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-des
 sudo dpkg -i /tmp/slack-desktop-4.28.184-amd64.deb
 
 #sudo apt install git git-lfs -y
-#sudo apt install libzip4 libtinfo5 libpython2.7
 
 #MySQL Workbench
 wget https://github.com/satyapraneet63/installation-files/raw/main/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb -P /tmp
-sudo apt install /tmp/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
+sudo dpkg -i /tmp/mysql-workbench-community_8.0.16-1ubuntu18.04_amd64.deb
 
 #Any_Desk Installing
 wget https://download.anydesk.com/linux/anydesk_6.2.0-1_amd64.deb -P /tmp
